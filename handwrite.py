da24ed3b6241b571c7f54a11b5eede7ef97e1411
HappyDustbunny <madshorn@operamail.com>
2019-11-18 10:51:08 +0100

    Shows numbers. Ish..

--- handwrite.py
+++ handwrite.py
@@ -1,4 +1,5 @@
 import os
+import time
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -20,25 +21,31 @@ def plot_stuff(picture_data, label_data):
 
     colour_data = picture_data[0: 784]
     print(len(colour_data), len(picture_data))
+    for colour in colour_data:
+        # fig.scatter(colour.reshape(28, 28), s=70, marker='s')  #, cmap='binary')
+        plt.matshow(colour.reshape(28, 28))
+        plt.show()
+        print(label_data[0:5])
+        time.sleep(1)
     # for yy in range(28):
     #     for xx in range(28):
-    n = 0
-    for number in colour_data:
-        for col in number:
-            colour = np.array([int(col), int(col), int(col)])
-            x = n % 28
-            y = int(n/28 % 28)
-            n += 1
-            # print('x, y, type(colour)', x, y, type(colour))
-            print('x, y, colour, type(colour)', x, y, colour, type(colour))
-            fig.scatter(x, 28 * y, s=70, c=colour, marker='s')  #, cmap='binary')
-        plt.show()
-            # https://ramdhaniverablog.wordpress.com/2016/09/30/
-            # scatter-plot-and-color-mapping-in-python/
-    for yyy in range(0, 30, 3):
-        fig.scatter(35, yyy, s=100, c=colour)
-    fig.set_aspect(1.0)
-    plt.show()
+    # n = 0
+    # for number in colour_data:
+    #     for col in number:
+    #         colour = np.array([int(col), int(col), int(col)])
+    #         x = n % 28
+    #         y = int(n/28 % 28)
+    #         n += 1
+    #         # print('x, y, type(colour)', x, y, type(colour))
+    #         print('x, y, colour, type(colour)', x, y, colour, type(colour))
+    #         fig.scatter(x, 28 * y, s=70, c=colour, marker='s')  #, cmap='binary')
+    #     plt.show()
+    #         # https://ramdhaniverablog.wordpress.com/2016/09/30/
+    #         # scatter-plot-and-color-mapping-in-python/
+    # for yyy in range(0, 30, 3):
+    #     fig.scatter(35, yyy, s=100, c=colour)
+    # fig.set_aspect(1.0)
+    # plt.show()
 
 def read_picture_data():
     file_name = os.path.join('.', 'datas', 'train-images.idx3-ubyte')
